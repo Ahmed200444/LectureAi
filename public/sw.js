031f81be856f3a9ef501d436e63bb4d461d2603c
Ahmed200444 <ahmedalkadi02@gmail.com>
2026-08-29 10:51:53 +0400

    Bump installed PWA cache for recorder audit release

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lectureai-shell-v5';
+const CACHE_NAME = 'lectureai-shell-v6';
 const CORE = ['/', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
