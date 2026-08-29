c205e03b83e3b2fc8c6b8c9ad40418d591786de6
Ahmed200444 <ahmedalkadi02@gmail.com>
2026-08-29 11:33:35 +0400

    Load iPhone iPad safe-area overrides

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ServiceWorkerRegister } from '../components/ServiceWorkerRegister';
 import '../styles/globals.css';
+import '../styles/mobile-ios.css';
 import LectureAI from './App';
 
 createRoot(document.getElementById('root')!).render(
