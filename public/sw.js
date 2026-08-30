5ffefcc282349f5fef3ee3d93830d84fd3ec8565
Ahmed200444 <ahmedalkadi02@gmail.com>
2026-08-30 18:34:48 +0400

    Refresh PWA cache for multilingual release

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lectureai-shell-v7';
+const CACHE_NAME = 'lectureai-shell-v8';
 const CORE = ['/', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
