b10d2389d4a999681c2878ac8e03e70ac8f93c6c
Ahmed200444 <ahmedalkadi02@gmail.com>
2026-08-31 12:53:30 +0400

    Inject native lecture transcription store

--- ios/LectureAIRecorder/Sources/LectureAIRecorderApp.swift
+++ ios/LectureAIRecorder/Sources/LectureAIRecorderApp.swift
@@ -3,11 +3,13 @@ import SwiftUI
 @main
 struct LectureAIRecorderApp: App {
     @StateObject private var recorder = RecorderStore()
+    @StateObject private var lectureStore = NativeLectureStore()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(recorder)
+                .environmentObject(lectureStore)
         }
     }
 }
